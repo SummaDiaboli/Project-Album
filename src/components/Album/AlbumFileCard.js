243aa8f96be6da69cfe53c2652a13f885064e40e
SummaDiaboli <salim.hussaini@backlinq.ng>
2019-08-22 19:06:02 +0100

    Fixes differenciate between pictures and videos

--- src/components/Album/AlbumFileCard.js
+++ src/components/Album/AlbumFileCard.js
@@ -1,22 +1,44 @@
 import React from 'react'
-import { Card, Image } from 'semantic-ui-react';
+import { Card, Image, Embed } from 'semantic-ui-react';
 // import PropTypes from 'prop-types'
 
 function AlbumFileCard(props) {
-    const image = props.url
+    const file = props.url
+    const extention = file.split('.').pop().split(/#|\?/g)[0]
 
-    return (
-        <Card
-            style={{
-                display: "inline-block",
-                padding: "10px",
-                margin: "1em"
-            }}
-            raised
-        >
-            <Image src={image} />
-        </Card>
-    )
+    if (
+        extention === 'gif' ||
+        extention === 'jpg' ||
+        extention === 'jpeg' ||
+        extention === 'png' ||
+        extention === 'tif' ||
+        extention === 'webp') {
+
+        return (
+            <Card
+                style={{
+                    display: "inline-block",
+                    padding: "10px",
+                    margin: "1em"
+                }}
+                raised
+            >
+                <Image src={file} />
+            </Card>
+        )
+    } else {
+        return (
+            <Card
+                style={{
+                    display: 'inline-block',
+                    padding: "10px",
+                    margin: "1em"
+                }}
+            >
+                <Embed url={file} />
+            </Card>
+        )
+    }
 }
 
 export default AlbumFileCard
